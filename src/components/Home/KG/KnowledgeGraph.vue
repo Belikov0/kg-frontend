10382491bdd84f0666d254f7d0870fa76458f576
liulianchadi <2011312527@qq.com>
2024-04-08 01:05:32 +0800

    graph: drag zoom text

--- src/components/Home/KG/KnowledgeGraph.vue
+++ src/components/Home/KG/KnowledgeGraph.vue
@@ -1,52 +1,58 @@
 <template>
     <div :class="[ns.b()]">
-        
 
-        <div id="KG"
-            :class="ns.e('kg')"
-        >
+        <div id="KG" :class="ns.e('kg')">
             <svg id="kg-svg"></svg>
         </div>
 
         <div :class="[ns.e('panel-wrapper')]">
             <FunctionPanel></FunctionPanel>
-        </div>  
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { useNamespace } from '@/utils/useNamespace';
-import createChart  from './chart.js'
-import createChart2 from './chart2.js';
-import data from '@/assets/data/graph.json'
+
+import createChart from './chart.js';
 import nodes from '@/assets/data/nodes.json'
 import links from '@/assets/data/relationship.json'
 
-let nodeList = nodes.map( a => {
+// 元素属性
+const props = defineProps({
+    width: {
+        type: Number
+    },
+    height: {
+        type: Number
+    }
+})
+
+let nodeList = nodes.map(a => {
     const b = {
         id: '',
         labels: '',
-        prop:{
+        prop: {
             name: '',
             description: '',
         }
     }
-    b.id = '' + (a.id )
+    b.id = '' + (a.id)
     b.labels = a.labels
     b.prop.name = a.prop.name
     b.prop.description = a.prop.description
     return b
 })
 
-let linkList = links.map( a => {
+let linkList = links.map(a => {
     const b = {
         source: '',
         target: '',
         type: ''
     }
-    b.source = '' + (a.source )
-    b.target = '' + (a.target )
+    b.source = '' + (a.source)
+    b.target = '' + (a.target)
     b.type = a.type
     return b
 })
@@ -57,28 +63,18 @@ import FunctionPanel from './FunctionPanel/FunctionPanel.vue';
 
 const ns = useNamespace('graph')
 
-onMounted(async () =>{ 
-    // createChart(data)
-     createChart2({
+onMounted(async () => {
+    createChart({
         nodes: nodeList,
         links: linkList
     })
 })
 
 
-const props = defineProps({
-    width: {
-        type: Number
-    },
-    height: {
-        type:Number
-    }
-})
+
 
 
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Home/KG/chart.js
+++ src/components/Home/KG/chart.js
@@ -1,6 +1,36 @@
 import * as d3 from 'd3'
 
-const createChart = (data: any) => {
+// interface Data {
+//     nodes: {
+//         id: string,
+//         labels: string,
+//         props: {
+//             name: string,
+//             description: string,
+//         }
+
+//     },
+//     links: {
+//         source: string,
+//         target: string,
+//         type: string
+//     }
+// }
+
+// 半径
+const radius = {
+    '课程': 50,
+    '章节': 40,
+    '节': 30,
+    '小节': 25
+}
+
+// 颜色
+
+
+
+
+const createChart = (data) => {
     // Specify the dimensions of the chart.
     const width = 928
     const height = 680
@@ -8,26 +38,23 @@ const createChart = (data: any) => {
     // Specify the color scale.
     const color = d3.scaleOrdinal(d3.schemeCategory10)
 
-    console.log(data.links)
     // The force simulation mutates links and nodes, so create a copy
     // so that re-evaluating this cell produces the same result.
-    const links = data.links.map((d: any) => ({ ...d }))
-    const nodes = data.nodes.map((d) => ({ ...d }) )
-
-    console.log(links)
-    console.log(nodes)
+    const links = data.links.map((d) => ({ ...d }))
+    const nodes = data.nodes.map((d) => ({ ...d }))
 
     // Create a simulation with several forces.
     const simulation = d3
         .forceSimulation(nodes)
         .force(
             'link',
-            d3.forceLink(links).id((d) => d.id)
+            d3.forceLink(links).id((d) => d.id).distance(200)
         )
         .force('charge', d3.forceManyBody())
         .force('x', d3.forceX())
         .force('y', d3.forceY())
-
+        .force('collide', d3.forceCollide(60).iterations(10))
+        .force('charge', d3.forceManyBody().strength(-200));
     // Create the SVG container.
     const svg = d3
         .select('#kg-svg')
@@ -39,54 +66,79 @@ const createChart = (data: any) => {
     // Add a line for each link, and a circle for each node.
     const link = svg
         .append('g')
-        .attr('stroke', '#999')
-        .attr('stroke-opacity', 0.6)
+        .attr('stroke', '#fff')
+        .attr('stroke-opacity', 0.9)
         .selectAll('line')
         .data(links)
         .join('line')
-        .attr('stroke-width', (d) => Math.sqrt(2))
+        .attr('stroke-width', () => Math.sqrt(30))
+        .text((d) => d.labels)
 
-    const node = svg
-        .append('g')
+    const g = svg.append('g')
+
+    const node = g
         .attr('stroke', '#fff')
         .attr('stroke-width', 1.5)
         .selectAll('circle')
         .data(nodes)
         .join('circle')
-        .attr('r', 5)
-        .attr('fill', (d) => color(d.group))
+        .attr('r', (d) => radius[d.labels])
+        .attr('fill', (d) => color(d.labels))
+        // .text((d) => d.properties.description)
+        
 
-    node.append('title').text((d) => d.id)
+    node.append('title').text((d) => d.prop.name)
 
     // Add a drag behavior.
     node.call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended))
+
+    // const g = node.selectAll('g')
+    const nodeText = g
+      .append('g')
+      .selectAll('text')
+      .data(nodes)
+      .join('text')
+      .attr('dy', '.3em')
+    //   .attr('class', 'node-name')
+      .attr('text-anchor', 'middle')
+      .style('pointer-events', 'none')
+      .style('color', '#000')
+    //   .attr('fill', '')
+      .text(function (d) {
+        console.log(d)
+        return d.prop.name;
+      });
 
     // Set the position attributes of links and nodes each time the simulation ticks.
     simulation.on('tick', () => {
         link.attr('x1', (d) => d.source.x)
             .attr('y1', (d) => d.source.y)
-            .attr('x2', (d) => d.target.x)
+            .attr('x2', (d) => {
+                return d.target.x
+            })
             .attr('y2', (d) => d.target.y)
 
         node.attr('cx', (d) => d.x).attr('cy', (d) => d.y)
+
+        nodeText.attr('x', (d) => d.x).attr('y', (d) => d.y)
     })
 
     // Reheat the simulation when drag starts, and fix the subject position.
-    function dragstarted(event: any) {
+    function dragstarted(event) {
         if (!event.active) simulation.alphaTarget(0.3).restart()
         event.subject.fx = event.subject.x
         event.subject.fy = event.subject.y
     }
 
     // Update the subject (dragged node) position during drag.
-    function dragged(event: any) {
+    function dragged(event) {
         event.subject.fx = event.x
         event.subject.fy = event.y
     }
 
     // Restore the target alpha so the simulation cools after dragging ends.
     // Unfix the subject position now that it’s no longer being dragged.
-    function dragended(event: any) {
+    function dragended(event) {
         if (!event.active) simulation.alphaTarget(0)
         event.subject.fx = null
         event.subject.fy = null
@@ -97,8 +149,16 @@ const createChart = (data: any) => {
     // stop naturally, but it’s a good practice.)
     // invalidation.then(() => simulation.stop())
 
+    const handleZoom = (e) => {
+        // d3.select('svg g').attr('transform', e.transform)
+        svg.attr('transform', e.transform)
+    }
+
+    let zoom = d3.zoom().on('zoom', handleZoom)
+
+    svg.call(zoom)
+
     return svg.node()
 }
 
-
-export default createChart as Function
+export default createChart
